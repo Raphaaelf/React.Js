2af9c9f1fceb98773fe1c95fc43e0e9718b697db
Raphael <raphae2010@hotmail.com>
2021-11-23 15:27:43 -0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './App.css';
+import { PostCard } from './components/PostCard';
 
 class App extends Component {
   state = {
@@ -26,23 +27,20 @@ class App extends Component {
     this.setState({ posts: postsAndPhotos });
   }
 
-
-
   render() {
   const { posts } = this.state;
-
 
   return (
     <section className="container">
       <div className="posts">
         {posts.map(post => (
-          <div className="post">
-            <img src={post.cover} alt={post.title} />
-            <div key={post.id} className="post-content">
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-            </div>
-          </div>
+          <PostCard 
+            // title={post.title}
+            // body={post.body}
+            // id={post.id}
+            // cover={post.cover}
+            post={post}
+          />
         ))}
       </div>
     </section>
