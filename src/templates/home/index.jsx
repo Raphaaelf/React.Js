93e668640f35228b03ad7b38efe796021b9f073a
Raphael <raphae2010@hotmail.com>
2021-11-23 19:12:17 -0300

    Imput

--- src/templates/home/index.jsx
+++ src/templates/home/index.jsx
@@ -3,13 +3,15 @@ import '../../styles/styles.css';
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../util/load-posts';
 import { Button } from '../../components/button';
+import { TextInput } from '../../components/testinput';
 
 export class Home extends Component {
   state = {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 10
+    postsPerPage: 2,
+    searchValue: ''
   };
 
   async componentDidMount() {
@@ -40,20 +42,49 @@ export class Home extends Component {
     this.setState({posts, page: nextPage});
   }
 
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value })
+  }
+
   render() {
-  const { posts, page, postsPerPage, allPosts } = this.state;
+  const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
   const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? 
+    allPosts.filter(post =>{
+      return post.title.toLowerCase().includes(searchValue.toLowerCase()
+      );
+    })
+   : posts;
 
   return (
     <section className="container">
-      <Posts posts={posts} />
+      {!!searchValue && (
+        <>
+        <h1>Search Value: {searchValue}</h1>
+        </>
+      )}
 
-      <div class="button-container">
-        <Button
+      <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+      <br /><br /><br />
+
+      {filteredPosts.length > 0 &&(
+        <Posts posts={filteredPosts} />
+      )}
+      {filteredPosts.length === 0 &&(
+        <p>Nao existem posts</p>
+      )}
+
+      <div className="button-container">
+        {!searchValue && (
+
+          <Button
           text="Load more posts" 
           onClick={this.loadMorePosts}
           disabled={noMorePosts}
-        />
+          />
+        )}
       </div>
     </section>
     )
